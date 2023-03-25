d36d3c27defdbe65d8f8eb55b81810048185c363
fc54989 <fc54989@alunos.fc.ul.pt>
2023-03-25 22:27:20 +0000

    MessagesDB and css corrected, login in process

--- chat.js
+++ chat.js
@@ -2,6 +2,11 @@
 const messageForm = document.querySelector('#message-form');
 const messageInput = document.querySelector('#message-input');
 const conversations = document.querySelector('#conversations');
+const userForm = document.querySelector('#user-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const phoneNumberInput = document.querySelector('#phone-number');
+const passwordInput = document.querySelector('#password');
 
 // Connect to the server using web sockets
 const socket = new WebSocket('ws://localhost:8080');
@@ -10,47 +15,119 @@ const socket = new WebSocket('ws://localhost:8080');
 socket.addEventListener('message', async event => {
   const messageText = await event.data.text();
   const message = JSON.parse(messageText);  
-  console.log(message);
-  // console.log("aloooo")
-  // console.log(message)
   displayMessage(message, message.sender);
 });
 
 // Send a message to the server
+// function sendMessage(text) {
+//   const message = {type: 'message', text , sender: "maria", receiver: "joao"};
+//   socket.send(JSON.stringify(message));
+//   displayMyMessage(message, "me");
+// }
+
 function sendMessage(text) {
-  const message = { text , sender: "maria"};
-  // console.log('ola');
-  // console.log(message);
+  const message = {text, sender: "maria", receiver: "joao"};
   socket.send(JSON.stringify(message));
   displayMyMessage(message, "me");
 }
 
+// Send user to the server
+// function sendUser(type, name, mail, phoneNumber, pass) {
+//   const user = {type, name, mail, phoneNumber, pass };
+//   socket.send(JSON.stringify(user));
+//   console.log("User sent to server");
+// }
+function sendUser( name, mail, phoneNumber, pass) {
+  const user = {name, mail, phoneNumber, pass };
+  socket.send(JSON.stringify(user));
+  console.log("User sent to server");
+}
+
 // Display a message in the conversation
 function displayMessage(message, sender) {
-  console.log('ola');
-  const messageElement = document.createElement('div');
-  console.log(message);
-  messageElement.classList.add('messageother');
-  messageElement.textContent = sender + ": " + message.text;
-  conversations.appendChild(messageElement);
+  const chatBubble = document.createElement("div");
+  chatBubble.classList.add("chat-bubble", "received");
+  chatBubble.textContent = sender + ": " + message.text;
+  conversations.appendChild(chatBubble);
+
+
+  // console.log('ola');
+  // const messageElement = document.createElement('div');
+  // console.log(message);
+  // messageElement.classList.add('messageother');
+  // messageElement.textContent = sender + ": " + message.text;
+  // conversations.appendChild(messageElement);
 }
 
 // Display my message in the conversation
 function displayMyMessage(message, sender) {
-  console.log('ola');
-  console.log(message);
-  const messageElement = document.createElement('div');
-  messageElement.classList.add('messageme');
-  messageElement.textContent = sender + ": " + message.text;
-  conversations.appendChild(messageElement);
+
+  const chatBubble = document.createElement("div");
+  chatBubble.classList.add("chat-bubble", "sent");
+  // chatBubble.classList.add('messageme');
+  chatBubble.textContent = sender + ": " + message.text;
+  conversations.appendChild(chatBubble);
+
+
+
+  // console.log('ola');
+  // console.log(message);
+  // const messageElement = document.createElement('div');
+  // messageElement.classList.add('messageme');
+  // messageElement.textContent = sender + ": " + message.text;
+  // conversations.appendChild(messageElement);
 }
 
-// Handle form submission
+//Handle message form submission
 messageForm.addEventListener('submit', event => {
   event.preventDefault();
   const text = messageInput.value.trim();
   if (text) {
     sendMessage(text);
-    messageInput.value = '';
+    messageInput.value = ''; // Clear the input field
   }
 });
+
+//SIGN UP PART
+
+// Handle user form submission from HTML form
+// userForm.addEventListener('submit', event => {
+//   event.preventDefault();
+//   const name = nameInput.value.trim();
+//   const mail = emailInput.value.trim();
+//   const phoneNumber = phoneNumberInput.value.trim();
+//   const pass = passwordInput.value.trim();
+
+//   if (name && mail && phoneNumber && pass) {
+//     //sendMessage(text);
+//     sendUser("user", name, mail, phoneNumber, pass);
+//     name.value = '';
+//     mail.value = '';
+//     phoneNumber.value = '';
+//     pass.value = '';
+//   }
+// });
+
+// Handle user form submission from the signup.html page
+
+
+
+
+//LOGIN PART
+
+// function login() {
+//   var phoneNumberInput = document.getElementById("phone-number").value;
+//   var passwordInput = document.getElementById("password").value;
+
+//   Ir buscar a base de dados os valores!!!!!
+//   var phoneNumber = "123456789";
+//   var password = "123456789";
+
+//   if (phoneNumberInput === phoneNumber && passwordInput === password) {
+//     alert("Login successful!");
+//     window.location = "app.html";
+//   } else {
+//     alert("Invalid username or password.");
+//   }
+//}
+
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const WebSocket = require('ws');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-
-//const mongoose = require('moongose');
-//import mongoose from 'mongoose';
+const uri = "mongodb+srv://modcgrupo6:modc2023@cluster0.tn70ola.mongodb.net/?retryWrites=true&w=majority";
+const clientDb = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const db = clientDb.db("MODC");
 
 const wss = new WebSocket.Server({ port: 8080 });
 
@@ -12,43 +12,88 @@ console.log("Server is Running")
 
 wss.on('connection', function connection(ws) {
   console.log('A new client has connected.');
-  clients.add(ws);
+  clients.add(ws);  
 
   ws.on('message', function incoming(message) {
     console.log(`Client sent message: ${message}`);
-
+  
+    // check the type of message received
+  // function checkType(message) {
+  //   const { type } = JSON.parse(message).type;
+  // }
+    
+    // if (checkType(message) == "message") {  
     // Send the message to all other clients
-    for (let client of clients) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        console.log(`Sending message to client`);
-        // LINHA ABAIXO N ENVIAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-
-        client.send(message);
-        console.log(`Is it sending?: ${message}`);
+      for (let client of clients) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          console.log(`Sending message to client`);
+          client.send(message);
+          // get the message content and the user who sent it
+          const { text, sender, receiver } = JSON.parse(message);
+          timestamp = new Date();
+          // Register the message in the database in json format
+          insertMessage(text, sender, receiver, timestamp);
+        }
       }
-    }
+  // } else {
+  //   const {type, name, email, phoneNumber, password } = JSON.parse(message);
+  //   insertUser(name, email, phoneNumber, password);
+  // }
   });
+
+  // ws.on('user', function incomingUser(user) {
+  //   console.log(`User created: ${user}`);
+  //   //Registar na base de dados
+  //   const { name, email, phoneNumber, password } = JSON.parse(user);
+  //   insertUser(name, email, phoneNumber, password);
+  // });
+  
 
   ws.on('close', function close() {
     console.log('A client has disconnected.');
     clients.delete(ws);
   });
 });
+ 
 
-async function insertMessage() {
-  const uri = "mongodb+srv://modcgrupo6:modc2023@cluster0.tn70ola.mongodb.net/?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function insertMessage(message, sender, receiver, timestamp) {
+
   try {
-    await client.connect();
+    await clientDb.connect();
     console.log("Database succesfully connected")
-    const db = client.db("MODC");
     const messages = db.collection("messages");
-    const result = await messages.insertOne({text: 'Hello, world!', sender: 'Alice', timestamp: new Date()})
+    const result = await messages.insertOne({messsage: message, sender: sender, receiver: receiver, timestamp: timestamp})
     console.log("Message added to the db")
   } finally {
-    await client.close();
+    await clientDb.close();
   }
 }
 
-insertMessage();
+// insert a user in the database
+async function insertUser(name, email, phoneNumber, password) {
+  try {
+    await clientDb.connect();
+    console.log("Database succesfully connected")
+    const users = db.collection("users");
+    const result = await users.insertOne({name: name, email: email, phoneNumber: phoneNumber, password: password})
+    console.log("User added to the db")
+  } finally {
+    await clientDb.close();
+  }
+}
+
+
+// async function insertUser(name, email, phoneNumber, password) {
+//   try {
+//     await clientDb.connect();
+//     console.log("Database succesfully connected")
+//     const messages = db.collection("users");
+//     const result = await messages.insertOne({name: name, email: email, phoneNumber: phoneNumber, password: password})
+//     console.log("User added to the db")
+//   } finally {
+//     await clientDb.close();
+//   }
+// }
+
+//insertMessage();
 // console.log("Server was killed")
